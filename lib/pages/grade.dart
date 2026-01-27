bf6297aac407a8ba2590652a97646cb246e1c965
Angelo <60273047+anweisen@users.noreply.github.com>
2026-01-27 23:44:56 +0100

    feat: implement selection for grade type & subject fields

--- lib/pages/grade.dart
+++ lib/pages/grade.dart
@@ -43,13 +43,53 @@ class _GradePageState extends State<GradePage> with AutomaticKeepAliveClientMixi
 
   GradeEntry get entry => GradeEntry(_grade!, _type!, _date!);
 
-  bool get isValid => _grade != null && _type != null && _date != null && _subject != null;
+  bool get isValid => _grade != null && _type != null && _date != null && _subject != null && _semester != null;
 
   bool get isFromExisting => widget.entry != null && widget.subject != null;
 
   void setGrade(int grade) {
     setState(() {
       _grade = grade;
+    });
+  }
+
+  void setSemester(Semester semester) {
+    // reset type if not valid anymore
+    if (_type != null && _subject != null) {
+      final choice = Provider.of<SettingsDataProvider>(context, listen: false).choice!;
+      final possibleTypes = GradeType.types(choice, _subject!, semester);
+      if (!possibleTypes.contains(_type)) {
+        _type = null;
+      }
+    }
+
+    setState(() {
+      _semester = semester;
+      _type = _type;
+    });
+  }
+
+  void setSubject(Subject subject) {
+    // reset type if not valid anymore
+    if (_type != null) {
+      final choice = Provider.of<SettingsDataProvider>(context, listen: false).choice!;
+      final possibleTypes = GradeType.types(choice, subject, _semester!);
+      if (!possibleTypes.contains(_type)) {
+        _type = null;
+      }
+    }
+    // reset semester if not valid anymore
+    if (_semester != null) {
+      final choice = Provider.of<SettingsDataProvider>(context, listen: false).choice!;
+      if (!choice.hasSubjectInSemester(subject, _semester!)) {
+        _semester = null;
+      }
+    }
+
+    setState(() {
+      _subject = subject;
+      _semester = _semester;
+      _type = _type;
     });
   }
 
@@ -73,124 +113,132 @@ class _GradePageState extends State<GradePage> with AutomaticKeepAliveClientMixi
   @override
   Widget build(BuildContext context) {
     super.build(context);
-    const double leftOffset = PageSkeleton.leftOffset;
     final ThemeData theme = Theme.of(context);
 
-    return Scaffold(
-      backgroundColor: Colors.transparent,
-      body: Stack(
-        children: [
-          Column(children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: leftOffset),
-              child:
-                  Row(crossAxisAlignment: CrossAxisAlignment.center, children: [
-                    Text("Note ${isFromExisting ? "ändern" : "hinzufügen"}", style: theme.textTheme.headlineMedium),
-              ]),
-            ),
-            const SizedBox(height: 24),
-            SizedBox(
-              height: 4 * (26 + 10), // height + spacing*2
-              child: ListView.builder(
-                padding: const EdgeInsets.symmetric(horizontal: leftOffset),
-                itemCount: 4,
-                itemBuilder: (context, indexI) => Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    for (int indexJ = 4 * indexI; indexJ < 4 * indexI + 4; indexJ++)
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-                        child: GestureDetector(
-                          onTap: () => setGrade(15 - indexJ),
-                          child: _buildGradeButton(indexJ, theme, _grade),
+    return SubpageSkeleton(
+      title: Row(crossAxisAlignment: CrossAxisAlignment.center, children: [
+        Text("Note ${isFromExisting ? "ändern" : "hinzufügen"}", style: theme.textTheme.headlineMedium),
+      ]),
+      actions: [
+        SaveButtonContainer(
+          btn1: SaveButton(
+            onTap: () {
+              if (isValid) {
+                SubpageController.of(context).closeSubpage(GradeEditResult(entry, _subject!, _semester!, false));
+              }
+            },
+            shown: isValid,
+            index: 0,
+            icon: Icons.chevron_right_rounded,
+            text: "Speichern",
+          ),
+          btn2: isFromExisting ? SaveButton(
+            onTap: () {
+              if (isValid) {
+                SubpageController.of(context).closeSubpage(GradeEditResult(entry, _subject!, _semester!, true));
+              }
+            },
+            shown: true,
+            index: 1,
+            icon: Icons.delete_forever,
+            text: "Löschen",
+          ) : null, shown: true,)
+      ],
+      children: [
+        Stack(
+          children: [
+            Column(children: [
+              SizedBox(
+                height: 4 * (26 + 10), // height + spacing*2
+                child: Column(
+                    children: [
+                      for (int indexI = 0; indexI < 4; indexI++)
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            for (int indexJ = 4 * indexI; indexJ < 4 * indexI + 4; indexJ++)
+                              Padding(
+                                padding: const EdgeInsets.symmetric(vertical: 5),
+                                child: GestureDetector(
+                                  onTap: () => setGrade(15 - indexJ),
+                                  child: _buildGradeButton(indexJ, theme, _grade),
+                                ),
+                              ),
+                          ],
                         ),
-                      ),
-                  ],
+                    ]
                 ),
               ),
-            ),
-            const SizedBox(height: 50),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: leftOffset),
-              child: Column(
-                children: [
+              const SizedBox(height: 50),
 
-                  if (_subject == null)
-                    const GradeOptionPlaceholder(text: "Wähle ein Fach")
-                  else
-                    GradeOptionPlaceholder(
+              // Subject
+              SubpageTrigger(
+                createSubpage: () => SubjectSelectionPage(semester: _semester, subject: _subject),
+                callback: (subject) {
+                  if (subject is Subject) setSubject(subject);
+                },
+                child: (_subject == null)
+                    ? const GradeOptionPlaceholder(text: "Wähle ein Fach")
+                    : GradeOptionPlaceholder(
                         text: _subject!.name,
-                        icon: Container(
-                          height: 22,
-                          width: 22,
-                          decoration: BoxDecoration(borderRadius: BorderRadius.circular(8), color: _subject!.color),)
-                    ),
-                  // on subject/semester change: reset GradeType if need
-                  const SizedBox(height: 20),
-                  if (_subject == null)
-                    const GradeOptionPlaceholder(text: "Wähle ein Semester")
-                  else
-                    GradeOptionPlaceholder(
-                        text: _semester!.detailedDisplay,
-                        icon: Icon(Icons.account_tree_rounded, color: theme.primaryColor, size: 24)),
-                  const SizedBox(height: 20),
-                  GestureDetector(
-                      onTap: () => SubpageController.of(context).openSubpage(GradeTypSelectionPage(semester: _semester, subject: _subject, originalType: _type), callback: (type) => {
-                        if (type is GradeType) {
-                          setType(type)
-                        }
-                      }),
-                      child: (_type == null)
-                          ? const GradeOptionPlaceholder(text: "Wähle eine Prüfungsart")
-                          : GradeOptionPlaceholder(
-                              text: _type!.name,
-                              icon: Icon(Icons.label_rounded, color: theme.primaryColor, size: 24))),
-                  const SizedBox(height: 20),
-                  if (_date == null)
-                    const GradeOptionPlaceholder(text: "Wähle ein Datum")
-                  else
-                    GradeOptionPlaceholder(
-                        text: GradeHelper.formatDate(_date!),
-                        icon: Icon(Icons.calendar_month_rounded, color: theme.primaryColor, size: 24)),
-                ],
+                        icon: Container(height: 22, width: 22, decoration: BoxDecoration(borderRadius: BorderRadius.circular(8), color: _subject!.color),)
+                ),
               ),
-            ),
-          ]),
 
-          SaveButtonContainer(
-              btn1: SaveButton(
-                onTap: () {
-                  if (isValid) {
-                    SubpageController.of(context).closeSubpage(GradeEditResult(entry, _subject!, false));
-                  }
+              // Semester
+              const SizedBox(height: 20),
+              SubpageTrigger(
+                createSubpage: () => SemesterSelectionPage(subject: _subject),
+                callback: (semester) {
+                  if (semester is Semester) setSemester(semester);
                 },
-                leftOffset: leftOffset,
-                shown: isValid,
-                index: 0,
-                icon: Icons.chevron_right_rounded,
-                text: "Speichern",
-              ),
-              btn2: isFromExisting ? SaveButton(
-                onTap: () {
-                  if (isValid) {
-                    SubpageController.of(context).closeSubpage(GradeEditResult(entry, _subject!, true));
-                  }
-                },
-                leftOffset: leftOffset,
-                shown: true,
-                index: 1,
-                icon: Icons.delete_forever,
-                text: "Löschen",
-              ) : null, leftOffset: leftOffset, shown: true,)
-        ],
-      ),
+                child: (_semester == null)
+                    ? const GradeOptionPlaceholder(text: "Wähle ein Semester")
+                    : GradeOptionPlaceholder(
+                      text: _semester!.detailedDisplay,
+                      icon: Icon(Icons.account_tree_rounded, color: theme.primaryColor, size: 24),)
+                ),
+
+              // if (_semester == null)
+              //   const GradeOptionPlaceholder(text: "Wähle ein Semester")
+              // else
+              //   GradeOptionPlaceholder(
+              //       text: _semester!.detailedDisplay,
+              //       icon: Icon(Icons.account_tree_rounded, color: theme.primaryColor, size: 24)),
+
+              // GradeType
+              // on subject/semester change: reset GradeType if needed
+              const SizedBox(height: 20),
+              SubpageTrigger(
+                  createSubpage: () => GradeTypSelectionPage(semester: _semester, subject: _subject, originalType: _type),
+                  callback: (type) => {
+                    if (type is GradeType) setType(type)
+                  },
+                  child: (_type == null)
+                      ? const GradeOptionPlaceholder(text: "Wähle eine Prüfungsart")
+                      : GradeOptionPlaceholder(
+                          text: _type!.name,
+                          icon: Icon(Icons.label_rounded, color: theme.primaryColor, size: 24))),
+
+              // Date
+              const SizedBox(height: 20),
+              if (_date == null)
+                const GradeOptionPlaceholder(text: "Wähle ein Datum")
+              else
+                GradeOptionPlaceholder(
+                    text: GradeHelper.formatDate(_date!),
+                    icon: Icon(Icons.calendar_month_rounded, color: theme.primaryColor, size: 24)),
+            ]),
+          ],
+        )
+      ],
     );
   }
 
   Widget _buildGradeButton(int index, ThemeData theme, int? selectedGrade) {
     final int grade = 15 - index;
     return Container(
-      width: 56,
+      width: 48,
       height: 26,
       decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(8),
@@ -223,7 +271,7 @@ class GradeOptionPlaceholder extends StatelessWidget {
       children: [
         if (icon != null) icon!
         else Icon(Icons.add_circle_rounded, color: theme.textTheme.bodySmall?.color, size: 24),
-        const SizedBox(width: 16),
+        const SizedBox(width: 12),
         Expanded(child: Text(text, softWrap: false, overflow: TextOverflow.ellipsis, maxLines: 2, style: Theme.of(context).textTheme.bodyMedium?.copyWith(color: textColor ?? (icon == null ? theme.textTheme.bodySmall?.color : null)))),
       ],
     );
@@ -264,14 +312,10 @@ class GradeTypSelectionPage extends StatelessWidget {
         break;
       }
     }
-    final List<GradeType> types = (subject != null && semester != null) ? GradeType.types(choice, subject!, semester!) : List.empty();
+    final List<GradeType> types = (subject != null && semester != null) ? GradeType.types(choice, subject!, semester!) : GradeType.values;
 
     return SubpageSkeleton(
-        title: Row(
-          children: [
-            Text("Prüfungsart wählen", style: theme.textTheme.headlineMedium),
-          ],
-        ),
+        title: Text("Prüfungsart wählen", style: theme.textTheme.headlineMedium),
         children: [
           for (int index = 0; index < types.length; index++)
             Padding(
@@ -337,19 +381,94 @@ class GradeTypSelectionPage extends StatelessWidget {
   }
 }
 
+class SubjectSelectionPage extends StatelessWidget {
+  const SubjectSelectionPage({super.key, required this.semester, this.subject});
+
+  final Semester? semester;
+  final Subject? subject;
+
+  @override
+  Widget build(BuildContext context) {
+    final ThemeData theme = Theme.of(context);
+
+    final choice = Provider.of<SettingsDataProvider>(context).choice!;
+    final subjects = choice.subjectsToDisplayForSemester(semester ?? Semester.q12_1);
+
+    return SubpageSkeleton(
+        title: Text("Fach wählen", style: theme.textTheme.headlineMedium),
+        children: [
+          for (int index = 0; index < subjects.length; index++)
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 7),
+              child: GestureDetector(
+                onTap: () => SubpageController.of(context).closeSubpage(subjects[index]),
+                child: GradeOptionPlaceholder(
+                    text: subjects[index].name,
+                    icon: Container(
+                      height: 22,
+                      width: 22,
+                      decoration: BoxDecoration(borderRadius: BorderRadius.circular(8), color: subjects[index].color),)
+                ),
+              ),
+            )
+
+        ]);
+  }
+}
+
+class SemesterSelectionPage extends StatelessWidget {
+  const SemesterSelectionPage({super.key, required this.subject});
+
+  final Subject? subject;
+
+  @override
+  Widget build(BuildContext context) {
+    final ThemeData theme = Theme.of(context);
+
+    final choice = Provider.of<SettingsDataProvider>(context).choice!;
+    final semesters = subject != null ? choice.getSemestersForSubject(subject!) : Semester.qPhase;
+
+    return SubpageSkeleton(
+        title: Text("Semester wählen", style: theme.textTheme.headlineMedium),
+        children: [
+          for (int index = 0; index < semesters.length; index++)
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 7),
+              child: GestureDetector(
+                onTap: () => SubpageController.of(context).closeSubpage(semesters[index]),
+                child: GradeOptionPlaceholderIcon(
+                    text: semesters[index].detailedDisplay,
+                    icon: getIcon(semesters[index]))
+                ),
+              ),
+        ]);
+  }
+
+  static IconData getIcon(Semester semester) {
+    return switch (semester) {
+      Semester.q12_1 => Icons.looks_one_rounded,
+      Semester.q12_2 => Icons.looks_two_rounded,
+      Semester.q13_1 => Icons.looks_3_rounded,
+      Semester.q13_2 => Icons.looks_4_rounded,
+      Semester.seminar13 => Icons.article_rounded,
+      Semester.abi => Icons.school_rounded,
+      _ => Icons.account_tree_rounded,
+    };
+  }
+}
+
 class SaveButtonContainer extends StatelessWidget {
-  const SaveButtonContainer({super.key, required this.btn1, required this.btn2, required this.leftOffset, required this.shown});
+  const SaveButtonContainer({super.key, required this.btn1, required this.btn2, required this.shown});
 
   final SaveButton? btn1, btn2;
-  final double leftOffset;
   final bool shown;
 
   @override
   Widget build(BuildContext context) {
     return Positioned(
-      bottom: leftOffset / 1.25,
-      left: leftOffset,
-      right: leftOffset,
+      bottom: 24 + PageSkeleton.leftOffset,
+      left: PageSkeleton.leftOffset,
+      right: PageSkeleton.leftOffset,
       child: Row(
         children: [
           ..._buildContent(),
@@ -374,13 +493,11 @@ class SaveButtonContainer extends StatelessWidget {
 
 class SaveButton extends StatelessWidget {
   const SaveButton({super.key,
-    required this.leftOffset,
     required this.shown,
     required this.onTap,
     required this.index, required this.icon, required this.text});
 
   final int index;
-  final double leftOffset;
   final bool shown;
   final Function() onTap;
   final IconData icon;
@@ -417,7 +534,8 @@ class SaveButton extends StatelessWidget {
 class GradeEditResult {
   final GradeEntry entry;
   final Subject subject;
+  final Semester semester;
   final bool remove;
 
-  GradeEditResult(this.entry, this.subject, this.remove);
+  GradeEditResult(this.entry, this.subject, this.semester, this.remove);
 }

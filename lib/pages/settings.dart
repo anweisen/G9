1c53300cfe0c7e9b2bd4f1381188a5d9ccea606d
Angelo <60273047+anweisen@users.noreply.github.com>
2026-02-25 03:06:07 +0100

    feat: add login button

--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import '../api/api.dart';
+import '../provider/account.dart';
 import '../provider/settings.dart';
+import '../widgets/subpage.dart';
 import '../widgets/skeleton.dart';
 import 'setup.dart';
 
@@ -12,13 +15,83 @@ class SettingsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     final ThemeData theme = Theme.of(context);
 
-    var choice = Provider.of<SettingsDataProvider>(context).choice!;
+    var accountProvider = Provider.of<AccountDataProvider>(context);
+    var choice = Provider.of<SettingsDataProvider>(context).choice;
 
     return PageSkeleton(
         title: const PageTitle(title: "Präferenzen"),
         children: [
-          const SizedBox(height: 10),
-          ...SetupFinishPage.buildSubjects(choice, theme),
+          const SizedBox(height: 8),
+
+          if (accountProvider.userProfile != null && accountProvider.accessToken != null)
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 18, vertical: 8),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                border: Border.all(color: theme.dividerColor, width: 4),
+              ),
+              child: Row(
+                children: [
+                  Row(
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(18),
+                        child: Image.network(accountProvider.userProfile!.picture, width: 44, height: 44, errorBuilder: (context, error, stackTrace) => const Icon(Icons.account_circle_rounded, size: 40))
+                      ),
+                      const SizedBox(width: 16,),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text("angemeldet als", style: theme.textTheme.bodySmall),
+                          Text(accountProvider.userProfile!.name, style: theme.textTheme.bodyMedium),
+                        ],
+                      )
+                    ],
+                  ),
+                  const Spacer(),
+                  GestureDetector(
+                    onTap: () => accountProvider.logout(),
+                    child: Icon(Icons.logout_rounded, size: 24, color: theme.indicatorColor,)
+                  )
+                ],
+              ),
+            )
+          else
+            GestureDetector(
+              onTap: () => Api.handleGoogleAuth(accountProvider),
+              child: Container(
+                padding: const EdgeInsets.symmetric(horizontal: 18, vertical: 8),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  border: Border.all(color: theme.dividerColor, width: 4),
+                ),
+                child: Row(
+                  children: [
+                    Row(
+                      children: [
+                        Icon(Icons.account_circle_rounded, size: 38, color: theme.shadowColor,),
+                        const SizedBox(width: 10,),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text("anmelden", style: theme.textTheme.bodyMedium?.copyWith(height: 1.2)),
+                            Text("mit Google", style: theme.textTheme.bodySmall),
+                          ],
+                        )
+                      ],
+                    ),
+                    const Spacer(),
+                    Icon(Icons.login_rounded, size: 24, color: theme.primaryColor,)
+                  ],
+                ),
+              ),
+            ),
+
+          const SizedBox(height: 24),
+
+          if (choice != null)
+            ...SetupFinishPage.buildSubjects(choice, theme),
+
           const SizedBox(height: 30),
           LayoutBuilder(
             builder: (context, constraints) {

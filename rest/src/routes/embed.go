7c3a6d0ad17f4c454bb5924120091807a0ce5894
Angelo <60273047+anweisen@users.noreply.github.com>
2026-02-27 17:00:38 +0100

    feat: create extract user id helper

--- rest/src/routes/embed.go
+++ rest/src/routes/embed.go
@@ -1,7 +1,23 @@
 package routes
 
-import "rest/src/provider"
+import (
+	jwtware "github.com/gofiber/contrib/v3/jwt"
+	"github.com/gofiber/fiber/v3"
+	"go.mongodb.org/mongo-driver/v2/bson"
+	"rest/src/provider"
+)
 
 type AppEmbed struct {
 	Database provider.Database
+}
+
+func ExtractUserId(ctx fiber.Ctx) (provider.UserId, error) {
+	jwt := jwtware.FromContext(ctx)
+	sub, err := jwt.Claims.GetSubject()
+	if err != nil {
+		return bson.NilObjectID, err
+	}
+
+	userId, err := bson.ObjectIDFromHex(sub)
+	return userId, err
 }

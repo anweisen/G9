8d125af0c1dd28b18edfce08300d30d0b09dafb9
Angelo <60273047+anweisen@users.noreply.github.com>
2026-03-01 03:31:22 +0100

    feat: create golang format adapter for dates

--- lib/adapter/json_converters.dart
+++ lib/adapter/json_converters.dart
@@ -13,3 +13,21 @@ class DateOnlyConverter implements JsonConverter<DateTime, String> {
     return object.toIso8601String().split('T').first;
   }
 }
+
+class GoDateTimeConverter implements JsonConverter<DateTime, String> {
+  const GoDateTimeConverter();
+
+  @override
+  DateTime fromJson(String json) {
+    // DateTime.parse is smart enough to handle strings with or without 'Z'
+    // and will return a local or UTC time accordingly.
+    return DateTime.parse(json).toUtc();
+  }
+
+  @override
+  String toJson(DateTime date) {
+    // 1. .toUtc() ensures the string ends with 'Z'
+    // 2. .toIso8601String() ensures the 'T' separator is used instead of a space
+    return date.toUtc().toIso8601String();
+  }
+}
